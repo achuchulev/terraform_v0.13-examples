6d5184fe7faabe2509309278724dab42cfb5572a
Atanas Chuchulev <atanas.v4@gmail.com>
2020-08-13 10:40:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ module "test" {
   source = "./modules/null-module"
   env    = "test"
 }
+
+resource "null_resource" "multi_env" {
+    provisioner "local-exec" {
+    command = "echo This is a test!"
+  }
+}
