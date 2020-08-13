042785fe2795314e5520c6c61325cb0ab74e88b1
Atanas Chuchulev <atanas.v4@gmail.com>
2020-08-13 15:32:39 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    null = {
+      # The "hashicorp" namespace is the new home for the HashiCorp-maintained
+      # provider plugins.
+      #
+      # source is not required for the hashicorp/* namespace as a measure of
+      # backward compatibility for commonly-used providers, but recommended for
+      # explicitness.
+      source  = "hashicorp/null"
+      version = "~> 2.0"
+    }
+}
+
 module "test" {
   source = "./modules/null-module"
   env    = "test"
